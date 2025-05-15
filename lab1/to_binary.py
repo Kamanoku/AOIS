c59bd32e0fb2396ebc0f8d6a5df601ad3da54161
Kamanoku <145385006+Kamanoku@users.noreply.github.com>
2025-05-16 00:03:16 +0300

    Delete lab1/to_binary.py

--- lab1/to_binary.py
+++
@@ -1,7 +0,0 @@
-def to_binary(num, bits=8):
-    """Преобразует число в двоичный формат (без знака)."""
-    binary = []
-    for i in range(bits):
-        binary.append(str(num % 2))
-        num //= 2
-    return ''.join(reversed(binary))
